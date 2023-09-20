6fe3a7560d209ecbeb2d16143339169232ae48ae
ascetik <ascetik@free.fr>
2023-09-20 19:26:26 +0200

    Traits for ScaleFactories

---
+++ src/Traits/UseIntermediateScales.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This is part of the UnitScale package.
+ *
+ * @package    UnitScale
+ * @category   Scale Factory trait
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
+namespace Ascetik\UnitscaleCore\Traits;
+
+use Ascetik\UnitscaleCore\Scales\CustomScale;
+
+trait UseIntermediateScales
+{
+    public function hecto()
+    {
+        return new CustomScale(2, 'h');
+    }
+
+    public function deca()
+    {
+        return new CustomScale(1, 'da');
+    }
+
+
+    public function deci()
+    {
+        return new CustomScale(-1, 'd');
+    }
+
+    public function centi()
+    {
+        return new CustomScale(-2, 'c');
+    }
+}
