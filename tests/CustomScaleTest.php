f0d198d3608000ac774ce6b5f050ade9bd0cf288
ascetik <ascetik@free.fr>
2023-09-20 18:03:29 +0200

    first tests with Scale : forward and backward

--- tests/CustomScaleTest.php
+++ tests/CustomScaleTest.php
@@ -1,15 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 use Ascetik\UnitscaleCore\Scales\CustomScale;
 use PHPUnit\Framework\TestCase;
 
 class CustomScaleTest extends TestCase
 {
-    public function testBaseScale()
+    public function testBaseScaleForward()
     {
         $scale = new CustomScale(3, 'k');
 
         $this->assertSame(1000, $scale->forward(1));
+    }
+
+    public function testBaseScaleBackward()
+    {
+        $scale = new CustomScale(3, 'k');
+
         $this->assertSame(0.001,$scale->backward(1));
     }
 }
