c614625b2bb32766fc23c5377157572d4bf86933
ascetik <ascetik@free.fr>
2023-09-22 19:20:56 +0200

    new ScaleWrapper : with factory method name and  associated Scaleassociated

---
+++ src/DTO/ScaleWrapper.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This is part of the UnitScale package.
+ *
+ * @package    UnitScale
+ * @category   Data Transfer Object
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
+namespace Ascetik\UnitscaleCore\DTO;
+
+use Ascetik\UnitscaleCore\Types\Scale;
+
+
+/**
+ * Carry a Scale attached to a name
+ * as identifier
+ *
+ * @version 1.0.0
+ */
+class ScaleWrapper
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly Scale $scale
+    ) {
+    }
+
+    public function contains(Scale $scale): bool
+    {
+        return $scale == $this->scale
+            ||
+            ($scale->factor() == $this->scale->factor()
+                &&
+                $scale->unit() == $this->scale->unit()
+            );
+    }
+}
