67c0cb4fd5924e7c2830f489c470c21260b797c2
ascetik <ascetik@free.fr>
2023-09-22 19:23:00 +0200

    some comments on the way to make things work...

--- src/Extensions/CustomFullValue.php
+++ src/Extensions/CustomFullValue.php
@@ -11,6 +11,29 @@ class CustomFullValue implements FullValue
     public function __construct(
         private CustomScaleValue $value
     ) {
+        /**
+         * Au dernier essai, on utilisait un ScaleReference.
+         * C'est toujours une bonne idée. Et du coup : on a deux choix :
+         * - soit on donne notre value en constructeur et il se demerde avec un trait pour gerer sa reference
+         * - soit c'est le ScaleValue qui forme le ScaleReference.
+         *
+         * Je préfère la première solution. ScaleValue n'a pas à s'occuper de la reference. Il ne connait que l'adjuster.
+         * Alors comme avant, c'est un trait qui va gérer ça.
+         * UseScaleReference me parait adapté.
+         *
+         * Et alors que gère la reference ?
+         * Voilà ce que j'ai récupéré de l'ancien package :
+         * - un ScaleContainer $container pour contenir tous les scales disponibles. initialisé au constructeur si pas donné
+         * - un Scale nullable $maxScale
+         * - un ScaleValue en public readonly
+         *
+         * C'est le FullValue qui doit gérer les appels from et to. L'opération est différente de nos amis les ScaleValues.
+         * C'est le ScaleReference qui enregistre les limites.
+         * Pour TimeScale j'avais prévu un from. Pour les CustomScales, c'est inutile. Il faut quand meme remettre en place ce qu'il faut pour ça.
+         * Et donc, je dois limiter les appels "magiques" à to.
+         * Pour le reste, je crois que le ScaleReference de base suffira. Il fonctionnait déjà très bien avant!
+         * Mais on refait le code. Meme le trait. Il y a qqs petites choses qui ont changé.
+         */
     }
 
     public function __toString(): string
