00251b4f565aade5a25d84d44c692cf049ef08e8
ascetik <ascetik@free.fr>
2023-10-04 17:50:06 +0200

    phpdoc

--- src/Parsers/ScaleCommandParser.php
+++ src/Parsers/ScaleCommandParser.php
@@ -1,11 +1,31 @@
 <?php
 
+/**
+ * This is part of the UnitScale Core package.
+ *
+ * @package    unitscale-core
+ * @category   Command parser
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
 declare(strict_types=1);
 
 namespace Ascetik\UnitscaleCore\Parsers;
 
+/**
+ * Parse prefixed commands
+ *
+ * @version 1.0.0
+ */
 class ScaleCommandParser
 {
+    /**
+     * Available prefixes registry
+     *
+     * @var array
+     */
     private readonly array $prefixes;
 
     public function __construct(string ...$prefixes)
@@ -13,6 +33,11 @@ class ScaleCommandParser
         $this->prefixes = $prefixes;
     }
 
+    /**
+     * Parse given command
+     *
+     * @param  string $method Method MUST start with any registered prefix, Exception thrown otherwise
+     */
     public function parse(string $method): string
     {
         foreach($this->prefixes as $prefix)
